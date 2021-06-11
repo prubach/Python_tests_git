81cedcd16cfef70bea4e9531f71452253848ac6f
Paweł Rubach <pawel.rubach@sorcersoft.com>
2021-06-11 13:44:54 +0200

    Initial commit

---
+++ primes.py
@@ -0,0 +1,16 @@
+n = 1000000
+
+def is_prime(i):
+    for j in range(2, int(i / 2) + 1):
+        if i % j == 0:
+            return False
+    return True
+
+
+def run(n=n):
+    for i in range(2, n):
+        if is_prime(i):
+            print(' ' + str(i), end='')
+
+
+run(100)
